b1c9430fb2e44a3156836e377dad8aa2183627b2
Chinmay Narayan <Chinmay@amuselabs.com>
2026-04-05 14:12:35 +0530

    testing by adding trailing slash

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
   url: 'https://amuselabs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/api-doc',
+  baseUrl: '/api-doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
