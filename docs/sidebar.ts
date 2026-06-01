0605f4a8989061997ff46d01d1289ada6cbd8433
Chinmay Narayan <Chinmay@amuselabs.com>
2026-06-01 12:45:13 +0530

    updated md files

--- docs/sidebar.ts
+++ docs/sidebar.ts
@@ -62,6 +62,18 @@ const sidebar: SidebarsConfig = {
     },
     {
       type: "category",
+      label: "Analytics",
+      items: [
+        {
+          type: "doc",
+          id: "get-series-specific-user-stats",
+          label: "Get series specific user stats",
+          className: "api-method get",
+        },
+      ],
+    },
+    {
+      type: "category",
       label: "User Fields",
       items: [
         {
@@ -75,18 +87,6 @@ const sidebar: SidebarsConfig = {
           id: "set-user-fields",
           label: "Set user fields",
           className: "api-method post",
-        },
-      ],
-    },
-    {
-      type: "category",
-      label: "User Stats",
-      items: [
-        {
-          type: "doc",
-          id: "get-series-specific-user-stats",
-          label: "Get series specific user stats",
-          className: "api-method get",
         },
       ],
     },
